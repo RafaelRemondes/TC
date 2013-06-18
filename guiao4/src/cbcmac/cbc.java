11efcfa10f31e5bd47f6952859f2915df7ae3515
rafaelremondes <rafael.remondes@gmail.com>
2013-06-18 02:29:55 +0100

    guiao4

--- guiao4/src/cbcmac/cbc.java
+++ guiao4/src/cbcmac/cbc.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package cbcmac;
+package src.cbcmac;
 
 import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
@@ -12,6 +12,7 @@ import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.cert.CertificateException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.logging.Level;
@@ -29,84 +30,104 @@ import javax.crypto.ShortBufferException;
  * @author rafaelremondes
  */
 public class cbc {
-  
-  
+
   static int n;
-  static byte[] iv;
-  static SecretKey key; 
+  static byte[] iv = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
+  static SecretKey key;
+  static byte[] plainText2 = "1234567812345678".getBytes();
   static byte[] plainText = "ifyourtimetoyouisworthsavingthenyoubetterstartswimmingoryouwillsinklikeastoneforthetimestheyareachanging".getBytes();
-  
-  
-   public static void genKey() throws NoSuchAlgorithmException, KeyStoreException, IOException, CertificateException {
-        KeyGenerator keyGen = KeyGenerator.getInstance("DES");
-		    key = keyGen.generateKey();
-	    }
+
+  public static boolean verifier(byte[] tag, List<byte[]> m) {
+    List<byte[]> tags = cbcMac(m, 0);
+    byte[] tag1 = tags.get(tags.size() - 1);
+    return Arrays.equals(tag, tag1);
+  }
+
+  public static void genKey() throws NoSuchAlgorithmException, KeyStoreException, IOException, CertificateException {
+    KeyGenerator keyGen = KeyGenerator.getInstance("DES");
+    key = keyGen.generateKey();
+  }
 
   public static byte[] enc(byte[] input, SecretKey key) throws NoSuchAlgorithmException, ShortBufferException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
-		Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
-    cipher.init(Cipher.ENCRYPT_MODE,key);   
+    Cipher cipher = Cipher.getInstance("DES/ECB/NoPadding");
+    cipher.init(Cipher.ENCRYPT_MODE, key);
     byte[] cipherText = cipher.doFinal(input);
     return cipherText;
   }
-  
-  public static List<byte[]> splitBlock(byte[] b,int i){
+
+  public static List<byte[]> splitBlock(byte[] b, int i) {
     List<byte[]> lista = new ArrayList<>();
     int x = 0;
-    while(x<b.length){
+    while (x < b.length) {
       int j = 0;
       byte[] array = new byte[8];
-      for(j=0;j<8;j++){
-        if(x+j<b.length){
-          array[j] =  b[x+j];
+      for (j = 0; j < 8; j++) {
+        if (x + j < b.length) {
+          array[j] = b[x + j];
         }
       }
       lista.add(array);
-      x+=j;
+      x += j;
     }
     return lista;
   }
-  
-  public static byte[] xor(byte[] a, byte[] b){
-    byte[] array = new byte[a.length];
-    System.out.println(new String(a));
-    System.out.println(new String(b));
 
-    for( int i = 0; i < a.length; i++ ) {
-      array[i] = (byte)(a[i] ^ b[i]);
+  public static byte[] xor(byte[] a, byte[] b) {
+    byte[] array = new byte[a.length];
+    for (int i = 0; i < a.length; i++) {
+      array[i] = (byte) (a[i] ^ b[i]);
     }
     return array;
   }
-  
-  public static byte[] aux(byte[] plainText, byte[] tag) throws NoSuchAlgorithmException, ShortBufferException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException{
+
+  public static byte[] aux(byte[] plainText, byte[] tag) throws NoSuchAlgorithmException, ShortBufferException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
     byte[] array = xor(plainText, tag);
-    array = enc(array,key);
+    array = enc(array, key);
     return array;
   }
-  
-  public static void genIV(){
-    iv = new byte[8];
+
+  public static byte[] genIV() {
+    byte[] b = new byte[8];
     Random rndm = new Random();
-    rndm.nextBytes(iv);
+    rndm.nextBytes(b);
+    return b;
   }
-  
-  public static byte[] cbcMac(List<byte[]> blocks){
-    byte[] tag = iv;
-    for(byte[] b : blocks){
+
+  public static List<byte[]> cbcMac(List<byte[]> blocks, int mod) {
+    List<byte[]> tags = new ArrayList<>();
+    byte[] tag = new byte[1];
+    if (mod == 1) {
+      tag = genIV();
+    } else {
+      tag = iv;
+    }
+    for (byte[] b : blocks) {
       try {
-        tag = aux(b,tag);
+        tag = aux(b, tag);
+        tags.add(tag);
       } catch (NoSuchAlgorithmException | ShortBufferException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException | InvalidKeyException ex) {
         Logger.getLogger(cbc.class.getName()).log(Level.SEVERE, null, ex);
       }
     }
-    return tag;
+    return tags;
   }
-    
-  public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, KeyStoreException, IOException, CertificateException{
+
+  public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, KeyStoreException, IOException, CertificateException {
     List<byte[]> blockList = splitBlock(plainText, 8);
+    List<byte[]> forgedMessage = new ArrayList<>();
     genKey();
-    genIV();
-    byte[] mac = cbcMac(blockList);
-    System.out.println(mac);
+    List<byte[]> tags1 = cbcMac(blockList, 0);
+    boolean v = verifier(tags1.get(tags1.size() - 1), blockList);
+    System.out.println("Real cbc-mac: " + v);
+    
+    byte[] fMessage = new byte[tags1.get(0).length];
+    for (int i = 1; i <= blockList.size()-1; i++) {
+      fMessage = xor(tags1.get(i-1),blockList.get(i));
+      forgedMessage.add(fMessage);
+    }
+    forgedMessage.add(xor(tags1.get(tags1.size()-1),blockList.get(0)));
+    v = verifier(tags1.get(0), forgedMessage);
+    System.out.println("Forged cbc-mac:" + v);
+
   }
-  
 }
